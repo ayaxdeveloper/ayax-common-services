7660a371f86eedd438b14c7a54dac85b3b9b12ed
maph maphskiy <maphskiy@gmail.com>
2018-01-26 12:00:23 +0300

    !

--- src/data/operation-service.ts
+++ src/data/operation-service.ts
@@ -8,18 +8,18 @@ export class OperationService extends BaseHttpService implements IOperationServi
         super(httpService);
     }
 
-    rawGet<T>(url: string): AxiosPromise<OperationResult<T>> {
+    get<T>(url: string): AxiosPromise<OperationResult<T>> {
         return this._http.get(url);
     }
 
-    rawPost<T>(url: string, data: any): AxiosPromise<OperationResult<T>> {
+    post<T>(url: string, data: any): AxiosPromise<OperationResult<T>> {
         return this._http.post(url, data);
     }
 
-    rawPut<T>(url: any, data: any): AxiosPromise<Operation> {
+    put<T>(url: any, data: any): AxiosPromise<Operation> {
         return this._http.put(url, data);
     }
-    rawDelete<T>(url: any): AxiosPromise<Operation> {
+    delete<T>(url: any): AxiosPromise<Operation> {
         return this._http.delete(url);
     }
 }

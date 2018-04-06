130365934837866a3764be0e86e73fd43ed252f8
maph maphskiy <maphskiy@gmail.com>
2018-04-06 09:23:38 +0300

    added baseUrl to operationService

--- src/data/base-http-service.ts
+++ src/data/base-http-service.ts
@@ -3,9 +3,11 @@ import { IHttpService } from "ayax-common-types";
 export abstract class BaseHttpService {
     _http: IHttpService;
     _postFix: string;
+    baseUrl: string;
 
     constructor(http: IHttpService, postFix?: string) {
         this._http = http;
+        this.baseUrl = http.baseUrl;
         if(postFix)
         this._postFix = postFix;
     }
